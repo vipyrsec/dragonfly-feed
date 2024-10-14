fd087e2a8b31cca4db896e6bf2a5fedfd03d1478
Robin5605 <robinjefferson123@gmail.com>
2024-10-14 01:49:17 -0500

    Add server action for reporting packages
    This will behave as a proxy between the client and Mainframe

---
+++ actions.ts
@@ -0,0 +1,20 @@
+'use server'
+
+import { getSession } from "@auth0/nextjs-auth0"
+import { DragonflyError, reportPackage, ReportPackageBody } from "./lib/dragonfly"
+
+export async function reportPackageAction(body: ReportPackageBody) {
+    const session = await getSession();
+    if(typeof session?.accessToken !== "string") return { error: "Not authenticated." };
+
+    try {
+        await reportPackage(body, session.accessToken);
+    } catch(e) {
+        // Server actions must return serializable errors to the client
+        if(e instanceof DragonflyError) {
+            return { error: `${e.statusCode}: ${e.body.detail}` }
+        }
+
+        throw e;
+    }
+}
